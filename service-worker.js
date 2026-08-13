1782381cc90926bf3d89cb4460c952776efbd0e5
mis388 <mis@metfabwireproducts.com>
2026-08-13 15:05:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,70 @@
-const CACHE_NAME = "weld-mesh-calculator-v4";
+const CACHE_NAME = "metfab-mesh-calculator-v5";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json",
-    "./metfab-logo.jpg"
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json",
+    "/metfab-logo.jpg",
+    "/icons/icon-192.png",
+    "/icons/icon-512.png",
+    "/icons/apple-touch-icon.png",
+    "/icons/favicon-32.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
 
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 return cache.addAll(FILES_TO_CACHE);
             })
     );
 
+    self.skipWaiting();
 });
 
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+
+                cacheNames.map(function (cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
 
     event.respondWith(
 
         caches.match(event.request)
-            .then(function(response) {
+            .then(function (cachedResponse) {
 
-                if (response) {
-                    return response;
+                if (cachedResponse) {
+                    return cachedResponse;
                 }
 
                 return fetch(event.request);
